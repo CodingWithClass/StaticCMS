da78e5d4b96140e3090886b65ef20b5a52d728a5
ClassCoder <classcoder@brenhamk-12.net>
2014-07-20 10:02:28 -0400

    Better timing
    Better timing in updating activeness of navbar item

--- homepage.js
+++ homepage.js
@@ -2,10 +2,11 @@ var currentPage = "home";
 var previousPage = null;
 
 function update() {
-	$("#body").load(currentPage +  "-module.html");
-	$("#page-" + previousPage).removeClass("active");
-	$("#page-" + currentPage).addClass("active");
-	previousPage = currentPage;
+	$("#body").load(currentPage +  "-module.html", function() {
+		$("#page-" + previousPage).removeClass("active");
+		$("#page-" + currentPage).addClass("active");
+		previousPage = currentPage;
+	});
 }
 
 function page(to) {
